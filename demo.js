62b969638ced3037c1c9bf79885d0866d5bc7897
Vedant Prabhu <vedantprabhu17@gmail.com>
2026-04-30 00:13:32 +0530

    Add initial demo.js

---
+++ demo.js
@@ -0,0 +1,11 @@
+function calculateTotal(price, tax) {
+    return price + tax;
+}
+const greet = (name) => {
+    console.log("Hello, " + name);
+};
+class User {
+    login() {
+        return "Logged in";
+    }
+}
